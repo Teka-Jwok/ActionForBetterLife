c5f2fe37d97e9e58f453d91445ffff8d2b392c4f
Tekajwok <46420989+Teka-Jwok@users.noreply.github.com>
2024-02-21 14:42:22 +0300

    Delete ActionForBetterLife directory

--- ActionForBetterLife/js/main.js
+++
@@ -1,9 +0,0 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-  var toggle = document.getElementById('toggle');
-  if (toggle.style.display == "none"){
-    toggle.style.display = "block";
-  } else {
-    toggle.style.display = "none";
-  }
-}
--- ActionForBetterLife/main/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (isset($_POST["username"], $_POST["password"])) {
-    	include_once("main.php");
-    	$username = $_POST["username"];
-    	$password = $_POST["password"];
-    	echo("You provide: ". $username." and ". $password);
-    }
-?>
